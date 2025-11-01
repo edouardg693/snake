8c6d669645140ed947ca357caa103493962153c2
Edouard Gardy <edouard.gardy@gmail.com>
2025-11-01 12:39:12 +0100

    Version samedi aprem, finie sauf super fruit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import random
+"""import random
 from pyray import *
 from raylib import *
+import time 
 SIDE = 40 #largeur d'un carreau
 WIDTH = 20
 HEIGHT = 20
+
 
 snake = [   #on représente le serpent
     [1,1],
@@ -28,10 +30,21 @@ while not window_should_close(): #tant qu'on ferme pas la fenêtre
     begin_drawing() #ça commence à dessiner
     clear_background(BLACK)    
     
-    #EN CAS DE FIN DE PARTIE
+    #ECRAN DE FIN DE PARTIE, RELANCE DU JEU
     if perdu:
-        draw_text("GAME OVER", (WIDTH * SIDE) // 8, (HEIGHT * SIDE) // 2, 100, RED)
+        draw_text("GAME OVER", (WIDTH * SIDE) // 8, (HEIGHT * SIDE) // 2 - 50, 100, RED)
         draw_text(f"Score : {s}", 5, 0, 50, WHITE)
+        draw_text("Appuie sur une touche pour rejouer", 90, (HEIGHT * SIDE) // 2 + 50 , 35, YELLOW)
+
+        # ➜ Si une touche est pressée ou si on clique, on relance le jeu
+        if get_key_pressed() != 0:
+            snake = [ [1,1],[2,1],[3,1]]
+            s=0 #score
+            vitesse = [0,1]
+            fruit = [WIDTH//2,HEIGHT//2]
+            super_fruit = [random.randint(0,WIDTH-1),random.randint(0,HEIGHT-1)]
+            compteur_super_fruit = 0
+            perdu = False
         end_drawing()
         continue 
 
@@ -89,7 +102,7 @@ while not window_should_close(): #tant qu'on ferme pas la fenêtre
         new_head[1]=0
     elif new_head in snake[:-1] :
         perdu = True
-    end_drawing()
+    end_drawing()"""
 
 
 
@@ -98,3 +111,114 @@ game over
 fruit circulaire
 super fruit
 jeu circulaire"""
+
+
+import random
+from pyray import *
+from raylib import *
+import time 
+
+SIDE = 40  # largeur d'un carreau
+WIDTH = 20
+HEIGHT = 20
+
+
+snake = [   # on représente le serpent
+    [1,1],
+    [2,1],
+    [3,1],
+]
+s = 0  # score
+vitesse = [0,1]
+fruit = [WIDTH//2, HEIGHT//2]
+super_fruit = [random.randint(0, WIDTH-1),
+                 random.randint(0, HEIGHT-1)]
+compteur_super_fruit = 0
+perdu = False
+
+init_window(SIDE * WIDTH, SIDE * HEIGHT, "Mon jeu")  # ouvre une fenêtre
+set_target_fps(60)  # maintenant 60 images par seconde
+
+# POUR FLUIDIFIER LE MOUVEMENT DU SERPENT
+nbre_frame = 0        
+pas_mouvement = 6  # fait que le serpent garde une vitesse assez lente malgré 60 FPS       
+
+while not window_should_close():
+    begin_drawing()
+    clear_background(BLACK)
+    
+    # ECRAN DE FIN DE PARTIE, RELANCE DU JEU
+    if perdu:
+        draw_text("GAME OVER", (WIDTH * SIDE) // 8, (HEIGHT * SIDE) // 2 - 50, 100, RED)
+        draw_text(f"Score : {s}", 5, 0, 50, WHITE)
+        draw_text("Appuie sur une touche pour rejouer", 90, (HEIGHT * SIDE) // 2 + 50, 35, YELLOW)
+
+        if get_key_pressed() != 0:
+            snake = [[1,1], [2,1], [3,1]]
+            s = 0
+            vitesse = [0,1]
+            fruit = [WIDTH//2, HEIGHT//2]
+            super_fruit = [random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1)]
+            compteur_super_fruit = 0
+            perdu = False
+            nbre_frame = 0
+        end_drawing()
+        continue
+
+    # DETECTION DES TOUCHES
+    if is_key_pressed(KEY_UP) and vitesse != [0,1]:
+        vitesse = [0,-1]
+    if is_key_pressed(KEY_DOWN) and vitesse != [0,-1]:
+        vitesse = [0,1]
+    if is_key_pressed(KEY_RIGHT) and vitesse != [-1,0]:
+        vitesse = [1,0]
+    if is_key_pressed(KEY_LEFT) and vitesse != [1,0]:
+        vitesse = [-1,0]
+
+    # DESSIN
+    for i, (x, y) in enumerate(snake):
+        color = GREEN if i == len(snake) - 1 else DARKGREEN
+        draw_rectangle(x * SIDE + 1, y * SIDE + 1, SIDE - 2, SIDE - 2, color)
+    draw_rectangle(fruit[0] * SIDE, fruit[1] * SIDE, SIDE, SIDE, BLUE)
+    draw_text(f"Score : {s}", 5, 0, 50, WHITE)
+    if compteur_super_fruit >= 5:
+        draw_rectangle(super_fruit[0] * SIDE, super_fruit[1] * SIDE, SIDE, SIDE, YELLOW)
+
+    # REGLAGE DU MOUVEMENT DU SERPENT A 60 FPS
+    nbre_frame += 1
+    if nbre_frame < pas_mouvement:
+        end_drawing()
+        continue
+    nbre_frame = 0  
+
+    # ANIMATION ET COMPTAGE DU SCORE
+    vx, vy = vitesse
+    hx, hy = snake[-1]
+    new_head = [hx + vx, hy + vy]
+
+    if new_head == fruit:
+        fruit = [random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)]
+        snake = snake + [new_head]
+        s += 10
+        compteur_super_fruit += 1
+    elif new_head == super_fruit:
+        super_fruit = [random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)]
+        snake = snake + [new_head]
+        s += 100
+        compteur_super_fruit = 0
+    else:
+        snake = snake[1:] + [new_head]
+
+    # JEU CIRCULAIRE ET CONDITION FIN DE PARTIE
+    if new_head[0] < 0:
+        new_head[0] = WIDTH
+    elif new_head[0] >= WIDTH:
+        new_head[0] = 0
+    elif new_head[1] < 0:
+        new_head[1] = HEIGHT
+    elif new_head[1] >= HEIGHT:
+        new_head[1] = 0
+    elif new_head in snake[:-1]:
+        perdu = True
+
+    end_drawing()
